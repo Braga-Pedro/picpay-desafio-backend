9d4a9a6bd42859137192a7cefd55a146ba9d3afb
Pedro <139722071+Braga-Pedro@users.noreply.github.com>
2025-01-08 00:08:24 -0300

    dividing laravel and spring challeng

---
+++ laravel/anotations/Links.md
@@ -0,0 +1,4 @@
+# Links para desenvolvimento do projeto
+
+## Kipper resolvendo o desafio
+https://www.youtube.com/watch?v=QXunBiLq2SM&t=139s&ab_channel=FernandaKipper%7CDev
---
+++ spring/picpay/src/test/java/picpay/challenge/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package picpay.challenge.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b4ca94a788b764d4dc2fe738abb8720d24725d47
Pedro <139722071+Braga-Pedro@users.noreply.github.com>
2025-01-12 21:14:52 -0300

    add length for string columns

--- spring/picpay/src/main/java/picpay/challenge/spring/domain/users/User.java
+++ spring/picpay/src/main/java/picpay/challenge/spring/domain/users/User.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
@@ -19,24 +22,28 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // gera o id de forma incremetal (i++)
     private Long id;
     // name: [string(50), not null]
+    @Column(length = 100)
     private String name;
     // email: [string(150), unique, not null]
-    @Column(unique = true)
+    @Column(unique = true, length = 100)
     private String email;
     // password: [string(100), not null]
     private String password;
     // document: [string(40), unique, not null]
-    @Column(unique = true)
+    @Column(unique = true, length = 50)
     private String document;
     // level: [enum[seller, common]] | criar Enum Level
     public enum Level { // criar classe type com method public enum Level
-        SELLER, COMMOM
+        SELLER, COMMON
     }
     @Enumerated(EnumType.STRING)
     private Level level;
 
+    @CreationTimestamp
+    @Column(updatable = false)
     private LocalDateTime created_at;
 
+    @UpdateTimestamp
     private LocalDateTime updated_at;
     // deleted_at:
     private LocalDateTime deleted_at;
